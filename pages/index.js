938bbf229204335d0b8b9eaad207ef5ec2c76701
Mario Bermudez Jr <mario.bermudez.jr@outlook.com>
2019-03-10 16:53:43 -0700

    feature: Add styles to jsx

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,14 @@ import fetch from 'isomorphic-unfetch';
 
 import Layout from '../components/MyLayout';
 
+function getPosts() {
+  return [
+    { id: 'hello-nextjs', title: 'Hello Next.js' },
+    { id: 'learn-nextjs', title: 'Learn Next.js is awesome' },
+    { id: 'deploy-nextjs', title: 'Deploy apps with ZEIT' }
+  ];
+}
+
 const Index = props => (
   <Layout>
     <h1>Batman TV Shows</h1>
@@ -15,6 +23,30 @@ const Index = props => (
         </li>
       ))}
     </ul>
+    <style jsx>{`
+      h1,
+      a {
+        font-family: 'Arial';
+      }
+
+      ul {
+        padding: 0;
+      }
+
+      li {
+        list-style: none;
+        margin: 5px 0;
+      }
+
+      a {
+        text-decoration: none;
+        color: blue;
+      }
+
+      a:hover {
+        opacity: 0.6;
+      }
+    `}</style>
   </Layout>
 );
 
